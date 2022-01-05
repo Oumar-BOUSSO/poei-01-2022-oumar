613f65fcbd908964809e56a082a70aa8714cb30a
Oumar BOUSSO <oumaribra.bousso@gmail.com>
2022-01-05 10:38:53 +0100

    tp3

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -3,10 +3,27 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class tp1 {
+    WebDriver driver;
 
+    @BeforeMethod
+
+    public void setup(){
+        WebDriver driver = new ChromeDriver();//permet de charger la page google sans l'ouvrir
+
+        driver.get("https://amazon.fr");//permet d'ouvrir la page google
+        driver.manage().window().maximize();
+
+        //fermer les cookies
+        WebElement buttonCookies = driver.findElement(By.id("sp-cc-accept"));
+        buttonCookies.click();
+
+    }
+
+    //chercher la machine à raclette dans la barre de recherche Amazon
     @Test
     public void test1() {
         WebDriver driver = new ChromeDriver();//permet de charger la page google sans l'ouvrir
