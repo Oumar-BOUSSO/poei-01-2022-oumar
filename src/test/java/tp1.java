20456c8a74056e4c6cea1aae15298a319b71785f
Oumar BOUSSO <oumaribra.bousso@gmail.com>
2022-01-05 15:12:35 +0100

    Added testExplicitWait

--- src/test/java/tp1.java
+++ src/test/java/tp1.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -17,9 +19,6 @@ public class tp1 {
     public void setup(){
         driver = new ChromeDriver();//permet de charger la page google sans l'ouvrir
 
-        //Implicit wait 2 secondes ici
-        //Cette mméthode s'applique une fois pour mes tests
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
 
         driver.get("https://amazon.fr");//permet d'ouvrir la page google
         driver.manage().window().maximize();
@@ -55,7 +54,7 @@ public class tp1 {
         WebElement barreRecherche = driver.findElement(By.id("twotabsearchtextbox"));
         barreRecherche.sendKeys("machine a raclette");
         barreRecherche.sendKeys(Keys.ENTER);
-        
+
         //WebElement produit1 = driver.findElement(By.cssSelector("[cel_widget_id='MAIN-SEARCH_RESULTS-5']"));
        //produit1.click();
 
@@ -65,5 +64,17 @@ public class tp1 {
         WebElement ajoutPanier = driver.findElement(By.cssSelector("[data-action='dp-pre-atc-declarative']"));
         ajoutPanier.click();
 
+    }
+
+    @Test
+    public void testExplicitWait() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+        driver.findElement(By.id("nav-hamburger-menu")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".hmenu-item[data-menu-id='10']")));
+        driver.findElement(By.cssSelector(".hmenu-item[data-menu-id='10']")).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("ul.hmenu-visible > li:nth-child(3)")));
+        driver.findElement(By.cssSelector("ul.hmenu-visible > li:nth-child(3) > a")).click();
     }
 }
